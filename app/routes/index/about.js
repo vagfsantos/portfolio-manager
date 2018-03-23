cbc0be76eebb9fd277ae8539e5417082e2ca77fa
Vagner F Santos <santos.vagnerferreira@gmail.com>
2018-03-22 21:46:39 -0300

    Refactoring

--- app/routes/index/about.js
+++ app/routes/index/about.js
@@ -3,21 +3,13 @@ import Route from '@ember/routing/route';
 export default Route.extend({
 
   model() {
+
     return this.store.findAll('about')
   },
 
-  actions: {
+  getPageTitle() {
 
-    save() {
+    console.log(this.get('store').forEach)
 
-      console.log(this.get('store').findAll('about'))
-      return
-
-      this.store.createRecord('about', {
-        data: {
-          name: this.model.page_data
-        }
-      }).save()
-    }
   }
 });
